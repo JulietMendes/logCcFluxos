ae526836e56c7fa69b694e17e3a7f6ba4fdd4984
juliet <juliete@link3.com.br>
2022-07-22 00:57:47 -0300

    up

---
+++ src/br/com/dio/basecamp/Main.java
@@ -0,0 +1,50 @@
+package br.com.dio.basecamp;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i1 = 10;
+        int i2 = 20;
+        float f1 = 7.5f;
+        float f2 = 2.5f;
+        double d1 = 40.7d;
+        char c1 = 'x';
+        char c2 = 'y';
+
+        String s1 = "Juliet";
+        String s2 = "Tamires";
+        String s3 = "Juliana";
+        boolean b1 = true;
+        boolean b2 = false;
+
+        long l1 = 1595L;
+        long l2 = 8995L;
+        byte y1 = 1;
+        short h1 = 26;
+
+        System.out.println("i1 == i2 " + (i1 == i2));
+        System.out.println("i1 != i2 " + (i1 != i2));
+        System.out.println("i1 > i2 " + (i1 > i2));
+        System.out.println("i1 <= i2 " + (i1 <= i2));
+
+        System.out.println("f1 == f2 " + (f1 == f2));
+        System.out.println("f1 != f2 " + (f1 != f2));
+        System.out.println("f1 > f2 " + (f1 > f2));
+        System.out.println("f1 <= f2 " + (f1 <= f2));
+
+        System.out.println("c1 == c2 " + (c1 == c2));
+        System.out.println("c1 != c2 " + (c1 != c2));
+        System.out.println("c1 > c2 " + (c1 > c2));
+        System.out.println("c1 <= c2 " + (c1 <= c2));
+
+        System.out.println("s1 == s2" + s1 == s2);
+        System.out.println("s1 == s3" + s1 == s3);
+        System.out.println("s1 != s2 " + s1 != s2 );
+//        System.out.println(s1 >= s2);
+//        System.out.println(s1 < s2);
+
+
+    }
+
+}
